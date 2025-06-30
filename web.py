d653d5d3d2c52b84190910542f985d324ed722e8
Aravind <aravindds0552@gmail.com>
2025-06-30 12:41:02 +0530

    add and complete todo in web app

--- web.py
+++ web.py
@@ -2,11 +2,28 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos("todos.txt")
+
+def add_todo():
+     todo = st.session_state['new_todo'] + '\n'
+     todos.append(todo)
+     functions.write_todos("todos.txt",todos)
+
+
+
 st.title("My Todo App")
 st.subheader("se")
 st.write("this is app")
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos("todos.txt",todos)
+        del st.session_state[todo]
 
 
-st.text_input(label="",placeholder="Enter a new todo")
+
+
+st.text_input(label=" ",placeholder="Enter a new todo",on_change = add_todo,key="new_todo")
+
+
+st.session_state
